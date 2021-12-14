ebaff968faa7049a745d21209a64afe041aabcfc
msgreentea <msgreentea8@gmail.com>
2021-12-14 17:03:09 +0900

    on progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+<link rel="shortcut icon" href="{{ asset('/img/snoopy.svg') }}" type="image/x-icon">
 <title>Todo List</title>
 </head>
 
@@ -20,20 +21,19 @@
         <!-- date -->
             <table>
                 <tr>
-                <th></th>
-                <th>created date</th>
-                <th>task</th>
-                <th>update</th>
-                <th>delete</th>
+                    <th></th>
+                    <th>created date</th>
+                    <th>task</th>
+                    <th>update</th>
+                    <th>delete</th>
                 </tr>
                 <tr>
-                <td><img src="/img/favicons.png" alt=""></td>
-                <td>date</td>
-                <!-- <td>{{ ->date }}</td> -->
-                <td><input type="text" name="addedTask" value="addedTask"></td>
-                <td><button class="btn-update">update</button></td>
-                <td><button class="btn-delete">delete</button></td>
-                <td><input class="delete" type="hidden" name="addedTask" value="addedTask"></td>
+                    <td><img src="/img/favicons.png" alt=""></td>
+                    <td>date</td>
+                    <td><input type="text" name="addedTask" value="added task"></td>
+                    <td><button class="btn-update">update</button></td>
+                    <td><button class="btn-delete">delete</button></td>
+                    <td><input class="delete" type="hidden" name="addedTask" value="addedTask"></td>
                 </tr>
             </table>
         </div>
