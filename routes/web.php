91b1da49d3a6ac303059593a3f0541de9d0f77a9
msgreentea <msgreentea8@gmail.com>
2021-12-21 17:10:00 +0900

    confirm遷移画面のルートってpostやっけ…？

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
 Route::get('/', [TodoController::class, 'index'])->name('index');
+Route::post('/todo/confirm', [TodoController::class, 'confirm'])->name('confirm');
 Route::post('/todo/create', [TodoController::class, 'create'])->name('create');
-Route::get('/todo/confirm', [TodoController::class, 'confirm'])->name('confirm');
 Route::post('/todo/update', [TodoController::class, 'update'])->name('update');
 Route::post('/todo/delete', [TodoController::class, 'delete'])->name('delete');
 Route::get('/todo/find', [TodoController::class, 'tofind'])->name('tofind');
