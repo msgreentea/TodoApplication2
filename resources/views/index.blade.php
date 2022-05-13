260aad82c409df3681b4f2bcee8600451b95fe56
msgreentea <msgreentea8@gmail.com>
2022-05-13 18:35:14 +0900

    レスポンシブ

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,11 +26,11 @@
             <table class="create-table">
                 <tr>
                     <th>task : </th>
-                    <td><input type="text" name="content" placeholder="add something you need to do ~~"></td>
+                    <td label=""><input type="text" name="content" placeholder="add something you need to do ~~"></td label="">
                 </tr>
                 <tr>
                     <th>deadline : </th>
-                    <td><input type="text" name="deadline" placeholder="by when?"></td>
+                    <td label=""><input type="text" name="deadline" placeholder="by when?"></td label="">
                 </tr>
             </table>
         </div>
@@ -45,7 +45,8 @@
 @section('content')
 
     <div class="ListedTasks">
-        <table>
+        {{-- パソコン --}}
+        <table class="table-pc">
             <tr>
                 <th></th>
                 <th>created date</th>
@@ -57,16 +58,16 @@
             </tr>
             @foreach ($items as $item)
             <tr>
-                <td><img src="/img/favicons.png" alt=""></td>
-                {{-- <td>{{ $item->created_at }}</td> --}}
-                <td>{{ $item->created_at->format('Y-m-d') }} <span class="hms">{{ $item->created_at->format('H:i:s') }}</span></td>
+                <td><img src="/img/favicons.png" alt=""></td label="">
+                {{-- <td label="">{{ $item->created_at }}</td label=""> --}}
+                <td label="created date">{{ $item->created_at->format('Y-m-d') }} <span class="hms">{{ $item->created_at->format('H:i:s') }}</span></td>
                 {{-- update --}}
                 <form action="{{ route('update', ['id' => $item->id]) }}" method="post">
                     @csrf
-                    <td><input type="text" name="content" value="{{ $item->content }}"></td>
-                    <td><input type="text" name="deadline" value="{{ $item->deadline }}"></td>
+                    <td label="task"><input type="text" name="content" value="{{ $item->content }}"></td>
+                    <td label="deadline"><input type="text" name="deadline" value="{{ $item->deadline }}"></td>
                     {{-- status --}}
-                    <td>
+                    <td label="status">
                         <select name="status" id="select" class="select">
                             @if ($item->status === "new")
                                 <option value="new" selected>new</option>
@@ -88,6 +89,37 @@
                     <td><input type="hidden" name="content" value="{{ $item->deadline }}"></td>
                 </form>
             </tr>
+            @endforeach
+        </table>
+        {{-- スマホ --}}
+        <table class="table-smart-phone">
+            @foreach ($items as $item)
+            <form action="{{ route('update', ['id' => $item->id]) }}" method="post">
+            <tr>
+                <th>created date</th>
+                <td>{{ $item->created_at->format('Y-m-d') }}</td>
+                <th>task</th>
+                <td><input type="text" name="content" value="{{ $item->content }}"></td>
+                <th>deadline</th>
+                <td><input type="text" name="deadline" value="{{ $item->deadline }}"></td>
+            </tr>
+            <tr>
+                <td label="status">
+                    <select name="status" id="select" class="select">
+                        @if ($item->status === "new")
+                            <option value="new" selected>new</option>
+                        @else
+                            <option value="new">new</option>
+                        @endif
+
+                        <option value="inProgress" @if($item->status === "inProgress") selected @endif>in progress</option>
+                        <option value="completed" @if($item->status === "completed") selected @endif>completed</option>
+                    </select>
+                </td>
+                <td><button class="btn-update">update</button></td>
+            </tr>
+            </form>
+
             @endforeach
         </table>
     </div>
